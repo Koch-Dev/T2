fca43275bb6ac29e571f5babac9bc0f27f9ec452
chiranjibkoch <chiranjibkoch8761@gmail.com>
2024-02-01 20:08:02 +0530

    sᴇxʏ ʜᴜ ᴠᴀɪ ✨

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = 19077475
+API_HASH = "0e257194816e0920c1117c4cc437bf23"
+BOT_TOKEN = "5868706821:AAHy2s9RFxBwxdZtwaVF1bQytaeln8SN5fQ"
+MONGODB_URI = "mongodb+srv://zoney:DUCmiRGqljxtxUJv@zoney.lwlpxrk.mongodb.net/?retryWrites=true&w=majority"
+OWNER_ID = 5218610039
---
+++ main.py
@@ -0,0 +1,55 @@
+import unicodedata
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
+import re
+from pyrogram import Client, filters
+
+app = Client(
+    "zoney",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+async def get_admins(chat_id: int):
+    return [
+        member.user.id
+        async for member in app.iter_chat_members(
+            chat_id, filter="administrators"
+        )
+    ]
+
+def load_blacklist():
+    with open("blacklist.txt", "r") as file:
+        return [normalize_text(line.strip()) for line in file]
+
+def contains_blacklisted_word(text, blacklist):
+    return any(word.lower() in text.lower() for word in blacklist)
+
+def normalize_text(text):
+    return unicodedata.normalize('NFKD', text)
+
+def has_special_font(text):
+    special_font_regex = re.compile(r'[\u0000-\u001F\u007F-\u009F\u00AD\u0600-\u0605\u061C\u06DD\u070F\u17B4\u17B5\u200B-\u200D\u2028-\u202F\u2060-\u206F\uFEFF\uFFF9-\uFFFB]')
+    return bool(special_font_regex.search(text))
+
+blacklist = load_blacklist()
+delete_mode = True
+
+@app.on_message(filters.group)
+async def delete_blacklisted_messages(client, message):
+    try:
+        if message.text:
+            regular_font_text = normalize_text(message.text)
+            if contains_blacklisted_word(regular_font_text, blacklist) and delete_mode:
+                await message.delete()
+    except Exception as e:
+        print(f"Error processing message: {e}")
+
+@app.on_message(filters.command(["textblock"]) & filters.group & filters.user(OWNER_ID) & filters.reply)
+async def toggle_delete_mode(client, message):
+    global delete_mode
+    delete_mode = not delete_mode
+    mode_status = "ON" if delete_mode else "OFF"
+    await message.reply(f"Blacklist delete mode is now {mode_status}.")
+print("fuck you")
+app.run()
