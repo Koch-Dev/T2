db89e8497173eb2c6b9335c463edb0437e01db20
Chiranjib <93111779+Koch-Dev@users.noreply.github.com>
2024-02-01 20:14:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-API_ID = 19077475
-API_HASH = "0e257194816e0920c1117c4cc437bf23"
-BOT_TOKEN = "5868706821:AAHy2s9RFxBwxdZtwaVF1bQytaeln8SN5fQ"
-MONGODB_URI = "mongodb+srv://zoney:DUCmiRGqljxtxUJv@zoney.lwlpxrk.mongodb.net/?retryWrites=true&w=majority"
+API_ID = 
+API_HASH = ""
+BOT_TOKEN = ""
 OWNER_ID = 5218610039
